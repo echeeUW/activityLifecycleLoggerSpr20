5c48b8dd2ba017dd19701048c91f67a26c20f5b3
Eric Chee <eric.chee@bestbuy.com>
2020-04-21 20:44:18 -0700

    Save and restore instance state. Also new layout for landscape mode

--- app/src/main/java/com/ericchee/myactivitylifecyclelogger_spr20/MainActivity.kt
+++ app/src/main/java/com/ericchee/myactivitylifecyclelogger_spr20/MainActivity.kt
@@ -4,24 +4,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         private val TAG = MainActivity::class.java.simpleName
+
+        private const val OUT_COUNT = "out_count"
     }
+
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnClickMe.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-
-            startActivity(intent)
-        }
         Log.i(TAG, "onCreate()")
+
+        if (savedInstanceState != null) {
+            with(savedInstanceState) {
+                count = getInt(OUT_COUNT, -1)
+
+            }
+        } else {
+            count = 0
+        }
+
+        tvCount.text = "You've clicked:\n $count times"
+        btnLaunchSecond.setOnClickListener { navigateToSecondActivity() }
+
+        btnAdd.setOnClickListener {
+            count++
+            tvCount.text = "You've clicked:\n $count times"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        outState.putInt(OUT_COUNT, count)
+
+        super.onSaveInstanceState(outState)
     }
 
     override fun onStart() {
@@ -50,5 +74,16 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.i(TAG, "onDestroy()")
 
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart()")
+    }
+
+
+    private fun navigateToSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/ericchee/myactivitylifecyclelogger_spr20/SecondActivity.kt
+++ app/src/main/java/com/ericchee/myactivitylifecyclelogger_spr20/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.ericchee.myactivitylifecyclelogger_spr20
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -14,7 +15,9 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
         Log.i(TAG, "onCreate() - B")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         btnGoBack.setOnClickListener {
             finish()
@@ -46,5 +49,14 @@ class SecondActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.i(TAG, "onDestroy() - B")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
